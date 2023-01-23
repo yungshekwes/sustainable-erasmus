84c379295690c51e0ac29eb3816500e27b6cead6
droneinspection <abhishek2aa2@gmail.com>
2023-01-23 11:00:21 +0100

    [Major] Instructions page implemented
    --

--- pages/survey.js
+++ pages/survey.js
@@ -2,7 +2,11 @@
 
 function Survey() {
   return (
-    <h1>Survey</h1>
+    <main className="relative flex items-center h-screen px-6 text-white lg:px-32 bg-gradient-to-tr from-blue-700 to-blue-400">
+      <section className="w-full md:w-9/12 xl:w-8/12">
+        <h1 className="text-3xl font-bold lg:text-6xl text-inherit">TO BE IMPLEMENTED</h1>
+      </section>
+    </main>
   )
 }
 
